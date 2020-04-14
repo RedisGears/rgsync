65e0787d5f71310e32ee5c41fced1559def6af17
Meir Shpilraien (Spielrein) <meir@redislabs.com>
2020-04-14 12:02:09 +0300

    Fix issue with snowflake Connector (#13)
    * Fix issue with snowflake Connector
    
    * More fixes

--- rgsync/Connectors/sql_connectors.py
+++ rgsync/Connectors/sql_connectors.py
@@ -40,8 +40,8 @@ class SnowflakeSqlConnection(BaseSqlConnection):
         self.account = account
 
     def _getConnectionStr(self):
-        return 'snowflake://{user}:{password}@{account}/{db}'.format(user=self.username,
-                                                                     password=self.password,
+        return 'snowflake://{user}:{password}@{account}/{db}'.format(user=self.user,
+                                                                     password=self.passwd,
                                                                      account=self.account,
                                                                      db=self.db)
 
@@ -182,5 +182,5 @@ class OracleSqlConnector(BaseSqlConnector):
             self.exactlyOnceQuery = GetUpdateQuery(self.exactlyOnceTableName, 'id', ['id', 'val'], ['val'])
 
 class SnowflakeSqlConnector(OracleSqlConnector):
-    def __init__(self, OracleSqlConnector, tableName, pk, exactlyOnceTableName=None):
-        OracleSqlBackend.__init__(self, OracleSqlConnector, tableName, pk, exactlyOnceTableName)
+    def __init__(self, connection, tableName, pk, exactlyOnceTableName=None):
+        OracleSqlConnector.__init__(self, connection, tableName, pk, exactlyOnceTableName)
