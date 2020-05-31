2779fbd4115caf48e584fc53f16d89e1f346b1f5
rafie <rafi@redislabs.com>
2020-05-31 12:22:12 +0300

    fixes 16

--- sbin/system-setup.py
+++ sbin/system-setup.py
@@ -25,6 +25,7 @@ class RGSyncSetup(paella.Setup):
 
     def debian_compat(self):
         self.install("python3-psutil")
+        self.install("mysql-server")
 
     def redhat_compat(self):
         self.install("redhat-lsb-core")
