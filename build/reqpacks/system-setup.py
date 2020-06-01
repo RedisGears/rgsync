9b32defc034bf3578c074c7f30d2526d3e555441
rafie <rafi@redislabs.com>
2020-06-01 19:55:41 +0300

    fixes 32

--- build/reqpacks/system-setup.py
+++ build/reqpacks/system-setup.py
@@ -5,7 +5,7 @@ import os
 import argparse
 
 HERE = os.path.dirname(__file__)
-ROOT = os.path.abspath(HERE, "../..")
+ROOT = os.path.abspath(os.path.join(HERE, "../.."))
 sys.path.insert(0, os.path.join(ROOT, "deps/readies"))
 import paella
 
