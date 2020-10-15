d1bc001ecec8ba9c3805686b31478dc9af8d92b6
meir@redislabs.com <meir@redislabs.com>
2020-10-15 20:48:02 +0300

    added mysql example

---
+++ examples/mysql/example.py
@@ -0,0 +1,20 @@
+from rgsync import RGWriteBehind, RGWriteThrough
+from rgsync.Connectors import MySqlConnector, MySqlConnection
+
+'''
+Create MySQL connection object
+'''
+connection = MySqlConnection('demouser', 'Password123!', 'localhost:3306/test')
+
+'''
+Create MySQL persons connector
+'''
+personsConnector = MySqlConnector(connection, 'persons', 'person_id')
+
+personsMappings = {
+	'first_name':'first',
+	'last_name':'last',
+	'age':'age'
+}
+
+RGWriteBehind(GB,  keysPrefix='person', mappings=personsMappings, connector=personsConnector, name='PersonsWriteBehind',  version='99.99.99')
