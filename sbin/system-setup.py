e0850effd17fa388f5e8d5d361d9a919d5273146
Rafi Einstein <raffapen@outlook.com>
2020-12-17 15:07:58 +0200

    system-setup.py fix

--- sbin/system-setup.py
+++ sbin/system-setup.py
@@ -4,8 +4,10 @@ import sys
 import os
 import argparse
 
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.insert(0, os.path.join(ROOT, "deps/readies"))
+HERE = os.path.abspath(os.path.dirname(__file__))
+ROOT = os.path.abspath(os.path.join(HERE, ".."))
+READIES = os.path.join(ROOT, "deps/readies")
+sys.path.insert(0, READIES)
 import paella
 
 #----------------------------------------------------------------------------------------------
@@ -17,10 +19,10 @@ class RGSyncSetup(paella.Setup):
     def common_first(self):
         self.install_downloaders()
         self.setup_pip()
-        self.pip3_install("wheel virtualenv")
-        self.pip3_install("setuptools --upgrade")
+        self.pip_install("wheel virtualenv")
+        self.pip_install("setuptools --upgrade")
         
-        self.pip3_install("-r %s/deps/readies/paella/requirements.txt" % ROOT)
+        self.pip_install("-r %s/paella/requirements.txt" % READIES)
         self.install("git zip unzip")
 
     def debian_compat(self):
@@ -30,9 +32,7 @@ class RGSyncSetup(paella.Setup):
 
     def redhat_compat(self):
         self.install("redhat-lsb-core")
-
-        # enable utf8 locale
-        self.run("sed -i 's/^\(override_install_langs=\)/# \1/' /etc/yum.conf")
+        self.run("%s/bin/enable-utf8" % READIES)
         self.run("yum reinstall -y glibc-common")
 
     def fedora(self):
@@ -42,11 +42,11 @@ class RGSyncSetup(paella.Setup):
         self.install_gnu_utils()
 
     def common_last(self):
-        self.pip3_install("--no-cache-dir git+https://github.com/Grokzen/redis-py-cluster.git@master")
-        self.pip3_install("--no-cache-dir git+https://github.com/RedisLabsModules/RLTest.git@master")
-        self.pip3_install("git+https://github.com/RedisGears/gears-cli.git")
-        self.pip3_install("-r %s/requirements.txt" % ROOT)
-        self.pip3_install("PyMySQL")
+        self.pip_install("--no-cache-dir git+https://github.com/Grokzen/redis-py-cluster.git@master")
+        self.pip_install("--no-cache-dir git+https://github.com/RedisLabsModules/RLTest.git@master")
+        self.pip_install("git+https://github.com/RedisGears/gears-cli.git")
+        self.pip_install("-r %s/requirements.txt" % ROOT)
+        self.pip_install("PyMySQL")
 
 #----------------------------------------------------------------------------------------------
 
