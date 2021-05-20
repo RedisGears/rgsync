48b14ebac25c7dd435f5d04fc5de0a72b2d3d01e
meir@redislabs.com <meir@redislabs.com>
2021-05-20 12:05:34 +0300

    added db2 support

--- rgsync/Connectors/__init__.py
+++ rgsync/Connectors/__init__.py
@@ -1,11 +1,15 @@
 from .simple_hash_connector import SimpleHashConnector
-from .sql_connectors import MsSqlConnector, MySqlConnector, SQLiteConnection, OracleSqlConnector, SnowflakeSqlConnector, MsSqlConnection, MySqlConnection, OracleSqlConnection, SnowflakeSqlConnection, SQLiteConnector
+from .sql_connectors import MsSqlConnector, MySqlConnector, SQLiteConnection, OracleSqlConnector, SnowflakeSqlConnector, MsSqlConnection, MySqlConnection, OracleSqlConnection, SnowflakeSqlConnection, SQLiteConnector, BaseSqlConnection, BaseSqlConnector, DB2Connection, DB2Connector
 from .cql_connector import CqlConnector, CqlConnection
 from .redis_connector import RedisConnector, RedisConnection, RedisClusterConnection
 
 __all__ = [
     'SimpleHashConnector',
 
+    'BaseSqlConnection',
+    'BaseSqlConnector',
+    'DB2Connector',
+    'DB2Connection',
     'MsSqlConnector',
     'MySqlConnector',
     'OracleSqlConnector',
