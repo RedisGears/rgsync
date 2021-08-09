4dbf22a3d6be9df01e01d0c60eb3da71ebc23480
Chayim I. Kirshen <c@kirshen.com>
2021-08-09 11:55:33 +0300

    roled in mongo callers

--- rgsync/Connectors/__init__.py
+++ rgsync/Connectors/__init__.py
@@ -1,6 +1,7 @@
 from .simple_hash_connector import SimpleHashConnector
 from .sql_connectors import MsSqlConnector, MySqlConnector, SQLiteConnection, OracleSqlConnector, SnowflakeSqlConnector, MsSqlConnection, MySqlConnection, OracleSqlConnection, PostgresConnection, PostgresConnector, SnowflakeSqlConnection, SQLiteConnector
 from .cql_connector import CqlConnector, CqlConnection
+from .mongo_connector import MongoConnector, MongoConnection
 from .redis_connector import RedisConnector, RedisConnection, RedisClusterConnection
 
 __all__ = [
@@ -19,6 +20,9 @@ __all__ = [
 
     'CqlConnector',
     'CqlConnection',
+
+    'MongoConnector',
+    'MongoConnection',
 
     'RedisConnector',
     'RedisConnection',
--- rgsync/Connectors/mongo_connector.py
+++ rgsync/Connectors/mongo_connector.py
@@ -11,16 +11,44 @@ class MongoConnection():
 
     @property
     def user(self):
-        return self._user
+        return self._user() if callable(self._user) else self._user
 
     @property
-    def password(self):
-        return self._password
+    def passwd(self):
+        return self._passwd() if callable(self._passwd) else self._passwd
 
     @property
     def db(self):
-        return self._db
+        return self._db() if callable(self._db) else self._db
+
+    # @property
+    # def collection(self):
+    #     return self._collection
 
     @property
-    def collection(self):
-        return self._collection
+    def _getConnectionStr(self):
+        return "mongodb://{}:{}".format(self.user, self.passwd)
+
+    def Connect(self):
+        from pymongo import MongoClient
+        WriteBehindLog('Connect: connecting ConnectionStr={}'.format(self._getConnectionStr))
+        client = MongoClient(self._getConnectionStr, serverSelectionTimeoutMS=5000)
+        client.server_info()  # there is no other way to test a connection
+        WriteBehindLog('Connect: Connected')
+        return client[self.db]
+
+
+class MongoConnector:
+
+    def __init__(self, connection, tableName, pk, exactlyOnceTableName=None):
+        self.conection = connection
+        self.tableName = tableName
+        self.pk = pk
+        self.exactlyOnceTableName = exactlyOnceTableName
+        self.supportedOperations = [OPERATION_DEL_REPLICATE, OPERATION_UPDATE_REPLICATE]
+
+    def TableName(self):
+        return self.tableName
+
+    def PrimaryKey(self):
+        return self.pk
